890b933a4cda04acad22dfa7a205c73cecca6c5c
Imtraped <91901629+Imtraped@users.noreply.github.com>
2023-06-09 09:40:01 -0400

    a

--- main.py
+++ main.py
@@ -24,61 +24,63 @@ for size in ship_sizes:
     valid = False
     while not valid:
         x = random.randint(0, grid_size - 1)
-        y = random.randint(0, grid_size - 1)
-        orientation = random.choice(["horizontal", "vertical"])
+        y = random.randint(0, grid_size - 1) #chooses a random general area of where a ship can be
+        orientation = random.choice(["horizontal", "vertical"]) #randomly decides if the ship will be vertical or horziontal 
 
-        if orientation == "horizontal":
+        if orientation == "horizontal": #aligns ship to be  horziontal 
             if y + size <= grid_size and np.count_nonzero(grid[x, y:y + size]) == 0:
-                grid[x, y:y + size] = 1
+                grid[x, y:y + size] = 1 
                 valid = True
-        else:
+        else: #aligns ship to be vertical 
             if x + size <= grid_size and np.count_nonzero(grid[x:x + size, y]) == 0:
                 grid[x:x + size, y] = 1
                 valid = True
 
 hit_count = 0
 ships_sunk = 0
-totalclicks = 0 
+totalclicks = 0  #these three variables track variables that will change throughout the loop
 font = pygame.font.Font(None, 36)
 
-running = True
-print("hey welcome to SPLOOSH KABOOM your goal is to find all 6 ships and sink them ")
-while running:
+running = True #as long as running is true the code will run 
+print("hey welcome to SPLOOSH KABOOM your goal is to find all 6 ships and sink them ") 
+while running: #code is in a while true loop until win condition is met
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: 
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN and ships_sunk < 6:
-            if pygame.mouse.get_pressed()[0]:
-                pos = pygame.mouse.get_pos()
+            if pygame.mouse.get_pressed()[0]: #code registers player's click and finds the row and col where the player clicked
+                pos = pygame.mouse.get_pos() 
                 col = (pos[0] - grid_padding) // tile_size
                 row = (pos[1] - grid_padding) // tile_size
                 try:
-                    if grid[row][col] == 1:
+                    if grid[row][col] == 1: #using the value of row and col where the player click if it equals to 1 that means the player hit a ship
                         hit_count += 1
                         grid[row][col] = -1 
-                        totalclicks
+                        totalclicks += 1
                         print("KABOOM!")
-                    if hit_count == 20:
-                            ships_sunk = 6
+                    if hit_count == 20: #when hit count is 20 (the total amount of hits when you add all the ship totals together ) playe meets win condition 
+                            ships_sunk = 6 
                             if ships_sunk == 6:
                                 running = False
                                 print("Congratulations! You sunk all the ships!")
-                    elif grid[row][col] == 0:
+                                print("it took ya" + str(totalclicks)+ " clicks.")
+                    elif grid[row][col] == 0: #when the value of row and grid make a 0 it means the player missed 
                         grid[row][col] = -2
                         print("sploosh...")
-                except: 
+                        totalclicks += 1
+                except: #when player clicks out of bounds usually an error happens but using try and execept makes it so it doesn't break this is a temp solution until i find a way to fix it
                     print("ERROR: Out Of Bounds. That wasn't very splash kaboomy of you")
-    screen.fill(BLUE)
+    screen.fill(BLUE) #makes the default grid all blue 
 
     for row in range(grid_size):
-        for col in range(grid_size):
+        for col in range(grid_size): 
             x = col * tile_size + grid_padding
             y = row * tile_size + grid_padding
             pygame.draw.rect(screen, BLUE, (x, y, tile_size, tile_size), 1)
 
-            if grid[row][col] == -1:
+            if grid[row][col] == -1: #when ship is hit marked with green tile 
                 pygame.draw.rect(screen, GREEN, (x + 1, y + 1, tile_size - 2, tile_size - 2))
-            elif grid[row][col] == -2:
+            elif grid[row][col] == -2: #when ship is missed marked with red tile 
                 pygame.draw.rect(screen, RED, (x + 1, y + 1, tile_size - 2, tile_size - 2))
 
     pygame.display.flip()
